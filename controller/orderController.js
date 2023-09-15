d5a68bee2e0282369acfa92217f4d3e442d36ac5
Kanha <k.sydaphasavanh@laposte.net>
2023-09-15 18:11:23 +0200

    add sender to controller

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,11 +1,6 @@
 const sender = require("../public/javascripts/sender")
 
 const orderController = (req, res, next) => {
-    const { email, name, message, phone, fs } = req.body;
-    // console.log(req.body);
-    console.log('controller '+res);
-    console.log(email, name, message, phone, fs);
-    // res.status(200).json(req.body.file.path);
     sender(req, res) 
 
 };
